379894426d462f4715e126aadc276f8712590f28
Tanmaysg01 <Tanmaygarge@gmail.com>
2024-01-23 17:52:35 +0530

    role

--- role.tf
+++ role.tf
@@ -15,3 +15,29 @@ resource "aws_iam_role" "lambda-ex" {
     ]
   })
 }
+
+resource "aws_iam_policy" "lambda-ex-policy" {
+  name        = "lambda-ex-policy"
+  path        = "/"
+  description = "My test policy"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "lambda:Describe*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
+  policy_arn = aws_iam_policy.lambda-ex-policy.arn
+  role       = aws_iam_role.lambda-ex.name
+}
