866093e042ac709ae357728b6edd2ccf703eb62e
Tanmay Garge <tanmay.garge@godigitaltc.com>
2024-01-23 10:25:06 +0530

    provider

---
+++ bucket.tf
@@ -0,0 +1,17 @@
+resource "aws_s3_bucket" "example" {
+  bucket = "shell-lambda-bucket-001"
+}
+
+ data "archive_file" "hello" {
+  type        = "zip"
+  source_file = "hello.py"
+  output_path = "./hello.zip"
+}
+resource "aws_s3_object" "hello" {
+  depends_on = [ data.archive_file.hello ]
+  bucket = "shell-lambda-bucket-001"
+  key    = "hello.py"
+  source = "hello.zip"
+}
+
+
